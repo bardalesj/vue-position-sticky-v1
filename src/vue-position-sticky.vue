e0c4406d99266a45dd3237b0b201e49197012197
Jose <bardalesj126@gmail.com>
2023-04-04 12:33:18 -0600

    chore: watch offsetTop

--- src/vue-position-sticky.vue
+++ src/vue-position-sticky.vue
@@ -29,6 +29,11 @@ export default {
             type: String
         }
     },
+    watch: {
+        offsetTop() {
+            this.handleResize()
+        }
+    },
     data() {
         return {
             sticky: false,
